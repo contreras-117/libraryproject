61eee92af508a7c7e016c81ca5214b2f99629925
Armando Contreras <mandoco.97@gmail.com>
2024-07-28 14:07:51 -0700

    Added Routes and Security Routes for our Okta implementation. Also fixed typo issue within the scope of the oktaConfig file.

--- frontend/react-library/src/lib/oktaConfig.ts
+++ frontend/react-library/src/lib/oktaConfig.ts
@@ -1,8 +1,8 @@
 export const oktaConfig = {
-    clientId: '0oai9te6oovcVqMmd5d7',
+    clientId: `0oai9te6oovcVqMmd5d7`,
     issuer: 'https://dev-81572834.okta.com/oauth2/default',
     redirectUri: 'http://localhost:3000/login/callback',
-    scopes: ['openId', 'profile', 'email'],
+    scopes: ['openid', 'profile', 'email'],
     pkce: true,
     dissableHttpsCheck: true,
 }
