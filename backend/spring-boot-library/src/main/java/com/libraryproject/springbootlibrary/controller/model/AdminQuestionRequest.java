cbce23ec6358f469b43b3f4b7d3e12601ad6a453
contreras-117 <mandoco.97@gmail.com>
2024-08-26 10:24:59 -0700

    Added a new admin question request model for an admin put request.

---
+++ backend/spring-boot-library/src/main/java/com/libraryproject/springbootlibrary/controller/model/AdminQuestionRequest.java
@@ -0,0 +1,12 @@
+package com.libraryproject.springbootlibrary.controller.model;
+
+import lombok.Data;
+
+@Data
+public class AdminQuestionRequest {
+
+    private Long id;
+
+    private String response;
+
+}
