905baf5d49656218d251aa59fa89d66533b52968
contreras-117 <mandoco.97@gmail.com>
2024-08-26 10:26:10 -0700

    Added a new method that will allow an admin to set their response and close the ticket in our database.

--- backend/spring-boot-library/src/main/java/com/libraryproject/springbootlibrary/service/MessagesService.java
+++ backend/spring-boot-library/src/main/java/com/libraryproject/springbootlibrary/service/MessagesService.java
@@ -1,10 +1,13 @@
 package com.libraryproject.springbootlibrary.service;
 
+import com.libraryproject.springbootlibrary.controller.model.AdminQuestionRequest;
 import com.libraryproject.springbootlibrary.dao.MessageRepository;
 import com.libraryproject.springbootlibrary.entity.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -21,5 +24,18 @@ public class MessagesService {
         Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
         message.setUserEmail(userEmail);
         messageRepository.save(message);
+    }
+
+    public void putMessage(AdminQuestionRequest adminQuestionRequest, String userEmail) throws Exception {
+        Optional<Message> message = messageRepository.findById(adminQuestionRequest.getId());
+
+        if (!message.isPresent()) {
+            throw new Exception("Could not find a message with given ID in the database.");
+        }
+
+        message.get().setAdminEmail(userEmail);
+        message.get().setResponse(adminQuestionRequest.getResponse());
+        message.get().setClosed(true);
+        messageRepository.save(message.get());
     }
 }
