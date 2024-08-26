b6b3ac153b6b05fff66892aa10b4b441c10ba5dc
contreras-117 <mandoco.97@gmail.com>
2024-08-26 13:47:15 -0700

    Created a new request model that the backend will need from the frontend in order to make this update and add a new book in the database.

---
+++ backend/spring-boot-library/src/main/java/com/libraryproject/springbootlibrary/controller/model/AddBookRequest.java
@@ -0,0 +1,20 @@
+package com.libraryproject.springbootlibrary.controller.model;
+
+import lombok.Data;
+
+@Data
+public class AddBookRequest {
+
+    private String title;
+
+    private String author;
+
+    private String description;
+
+    private int copies;
+
+    private String category;
+
+    private String img;
+
+}
