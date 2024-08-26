d464940f8773a300987969999558fa2aa59d7c13
contreras-117 <mandoco.97@gmail.com>
2024-08-26 13:37:07 -0700

    Created a new function for the submit response button so that it makes a call to the function property being passed into this child component. The function property being passed in is a PUT request to the backend to submit a response to a pending question needing an answer.

--- frontend/react-library/src/layouts/ManageLibraryPage/components/AdminMessage.tsx
+++ frontend/react-library/src/layouts/ManageLibraryPage/components/AdminMessage.tsx
@@ -1,10 +1,22 @@
 import { useState } from "react";
 import MessagesModel from "../../../models/MessagesModel";
 
-export const AdminMessage: React.FC<{ message: MessagesModel }> = (props, key) => {
+export const AdminMessage: React.FC<{
+    message: MessagesModel,
+    submitResponseToQuestion: any
+}> = (props, key) => {
 
     const [displayWarning, setDisplayWarning] = useState(false);
     const [response, setResponse] = useState('');
+
+    function submitBtn() {
+        if (props.message.id !== null && response !== "") {
+            props.submitResponseToQuestion(props.message.id, response);
+            setDisplayWarning(false);
+        } else {
+            setDisplayWarning(true);
+        }
+    }
 
     return (
         <div key={props.message.id}>
@@ -27,7 +39,7 @@ export const AdminMessage: React.FC<{ message: MessagesModel }> = (props, key) =
                                 onChange={e => setResponse(e.target.value)} value={response}></textarea>
                         </div>
                         <div>
-                            <button type='button' className='btn btn-primary mt-3'>
+                            <button type='button' className='btn btn-primary mt-3' onClick={submitBtn}>
                                 Submit Response
                             </button>
                         </div>
