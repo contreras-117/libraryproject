32ed55f9c047269dfd823dea4e7a5961260130f4
contreras-117 <mandoco.97@gmail.com>
2024-08-26 15:45:29 -0700

    Created a new controller class with a post book method to be able to add a new book into our database.

---
+++ backend/spring-boot-library/src/main/java/com/libraryproject/springbootlibrary/controller/AdminController.java
@@ -0,0 +1,32 @@
+package com.libraryproject.springbootlibrary.controller;
+
+import com.libraryproject.springbootlibrary.controller.model.AddBookRequest;
+import com.libraryproject.springbootlibrary.service.AdminService;
+import com.libraryproject.springbootlibrary.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    private AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/api/admin")
+    public void postBook(@RequestHeader(value = "Authorization") String token,
+                         @RequestBody AddBookRequest addBookRequest) throws Exception {
+        String admin = ExtractJWT.payloadExtraction(token, "\"userType\"");
+
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+
+        adminService.postBook(addBookRequest);
+    }
+}
