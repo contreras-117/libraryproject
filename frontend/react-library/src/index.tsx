38b052deb88f4431b91525430942f10d2e02da52
contreras-117 <mandoco.97@gmail.com>
2024-08-29 14:13:24 -0700

    Implemented and installed Stripe dependencies on the frontend.

--- frontend/react-library/src/index.tsx
+++ frontend/react-library/src/index.tsx
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe('pk_test_51PswKqFbiNfEteC71HidSLH0vFC7ZgsExTNyB9BpKajXYZpHhq8lf2aKrOSY32Uj78D9KYPku1wXio2atY9JzgMD00AjwIb9Hi');
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <App />
+    <Elements stripe={stripePromise}>
+      <App />
+    </Elements>
   </BrowserRouter>
 );
